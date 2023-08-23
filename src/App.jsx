7005455f32721dd37e283efc90a6ac57596099e3
yastman <homeinfa@gmail.com>
2023-08-23 12:40:35 +0200

    add const logoImg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 const App = () => {
   const header = "Yaroslav";
+  const logoImg = "/logo512.png";
   return (
     <div className="App">
       <div className="Logo">
-        <img src="/logo512.png" alt="Logo" />
+        <img src={logoImg} alt="Logo" />
       </div>
       <h1>{header}</h1>
     </div>
