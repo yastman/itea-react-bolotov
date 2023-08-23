31eb1212b6b3838edc43dcf081e6ca3edc5004e7
yastman <homeinfa@gmail.com>
2023-08-23 12:36:03 +0200

    add logo to project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ const App = () => {
   const header = "Yaroslav";
   return (
     <div className="App">
+      <div className="Logo">
+        <img src="/logo512.png" alt="Logo" />
+      </div>
       <h1>{header}</h1>
     </div>
   );
